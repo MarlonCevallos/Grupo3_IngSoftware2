3588f1dc34033fc5187b4127ffa2788e2a0e1984
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2022-12-20 18:45:09 -0500

    formulario menu

--- Codigo_Web_MisDelicias/menu.php
+++ Codigo_Web_MisDelicias/menu.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Menú</title>
+    
     <!--<link rel="shortcut icon" href="favicon.png">-->
     <link rel="stylesheet" href="assets/css/estilos_menu.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
