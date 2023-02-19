bfb0216a0385b99ceda2ec20ca978d09fe56ff1e
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2023-02-18 21:43:44 -0500

    V1 - Registro de usuarios

--- Codigo_Web_MisDelicias/php/conexion_db.php
+++ Codigo_Web_MisDelicias/php/conexion_db.php
@@ -1,8 +1,7 @@
 <?php
-
 $conexion = mysqli_connect("localhost", "root", "", "misdelicias_db");
 if($conexion){
-    echo 'Se conectó correctamente a la BDD';
+    echo 'La conexión a la BDD se realizó con éxito';
 }else{
     echo 'Error no se pudo conectar a la BDD';
 }
