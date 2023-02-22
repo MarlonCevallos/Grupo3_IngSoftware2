41a2ec985fd4dcde52a87091023e2cc165ae9324
Roberto Carlos Pallo Yautibug <85329633+RobertoPallotech@users.noreply.github.com>
2023-02-21 21:01:23 -0500

    Update 1.0 index.php

--- Codigo_Web_MisDelicias/index.php
+++ Codigo_Web_MisDelicias/index.php
@@ -9,6 +9,44 @@
     
 </head>
 <body>
-        
+  <main>
+            <div class="contenedor__todo">
+                <div class="caja__trasera">
+                    <div class="caja__trasera-login">
+                        <h1>Mis Delicias</h1>
+                        <h3>¿Ya tienes una cuenta?</h3>
+                        <p>Inicia sesión para entrar en la página</p>
+                        <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+                    </div>
+                    <div class="caja__trasera-register">
+                        <h1>Mis Delicias</h1>
+                        <h3>¿Aún no tienes una cuenta?</h3>
+                        <p>Regístrate para que puedas iniciar sesión</p>
+                        <button id="btn__registrarse">Regístrarse</button>
+                    </div>
+                </div>
+                <!--Formulario de Login y registro-->
+                <div class="contenedor__login-register">
+                    <!--Login-->
+                    <form action="php/login.php" method="POST" class="formulario__login">
+                        <h2>Iniciar Sesión</h2>
+                        <input type="text" placeholder="Usuario" name="usuario" required="">
+                        <input type="password" placeholder="Contraseña" name="contraseña" required="">
+                        <button>Ingresar</button>
+                    </form>
+                    <!--Register-->
+                    <form action="php/registro_usuarios.php" method="POST" class="formulario__register" id="formulario">
+                        <h2>Registrarse</h2>
+                        <input type="text" placeholder="Nombre Completo" name="nombre" required="" pattern="[a-zA-Z ]{2,254}">
+                        <input type="email" placeholder="Correo Electronico" name="correo" required="" >
+                        <input type="text" placeholder="Dirección Entrega" name="direccion" required="">
+                        <input type="text" placeholder="Usuario" name="usuario" required="">
+                        <input type="password" placeholder="Contraseña" name="contraseña" required="" >
+                        <button>Regístrarse</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+        <script src="assets/js/script.js"></script>      
 </body>
 </html>
